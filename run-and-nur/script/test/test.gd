7b967075949b45be3caae65e4db08a2d24c6066f
Reboot Student <nestorlpa2003@gmail.com>
2024-09-16 10:45:46 +0100

    test_multiplayer_v0.1

---
+++ run-and-nur/script/test/test.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var peer : ENetMultiplayerPeer= ENetMultiplayerPeer.new()
+@onready var hud=$Hud
+
+# Called when the node enters the scene tree for the first time.
+func _on_host_pressed() -> void:
+	peer.create_server(3500,2)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	_on_peer_connected()
+	hud.visible=false
+
+func _on_join_pressed() -> void:
+	peer.create_client("localhost",3500)
+	multiplayer.multiplayer_peer = peer
+	hud.visible=false
+
+func _on_peer_connected(id: int =1) -> void:
+	var player_scene= load("res://scene/test/test_player.tscn")
+	var player_instantiate= player_scene.instantiate()
+	player_instantiate.name=str(id)
+	add_child(player_instantiate,true)
