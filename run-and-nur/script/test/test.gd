d2b41fe89e9c1b7f31396379ede55d6b8c1bc852
Reboot Student <nestorlpa2003@gmail.com>
2024-09-17 16:35:34 +0100

    prueba_online

--- run-and-nur/script/test/test.gd
+++ run-and-nur/script/test/test.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
-var peer : ENetMultiplayerPeer= ENetMultiplayerPeer.new()
-@onready var hud=$Hud
-var player_id
 
+@onready var hud=$Hud
+var player_id=1
 
 # Called when the node enters the scene tree for the first time.
 func _on_host_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(3500,2)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	_on_peer_connected()
 	hud.visible=false
+	print(player_id)
 
 func _on_join_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
 	peer.create_client("localhost",3500)
 	multiplayer.multiplayer_peer = peer
 	hud.visible=false
@@ -23,7 +25,33 @@ func _on_peer_connected(id: int =1) -> void:
 	var player_instantiate= player_scene.instantiate()
 	player_id=id
 	player_instantiate.name=str(id)
-	
+	print(player_id)
 	add_child(player_instantiate,true)
 
-#ver quien pulsa o no el boton 
+
+
+#asi se podrian diferenciar
+
+func _on_button_2_pressed() -> void:
+	if player_id==1:
+		rpc("player_1") 
+	else:
+		rpc("player_2") 
+
+#ESTO LO HAGO PORQUE ES LO UNICO QUE SE DE RPC EL QUE LLAMA AL METODO EN TODOS
+@rpc("any_peer","call_local")
+func player_1() :
+	$Sprite2D.scale=Vector2(2,2)
+
+@rpc("any_peer","call_local")
+func player_2() :
+	$Sprite2D.scale=Vector2(1,1)
+
+#esto hace que se llamen en todos los ordenadores a la vez
+func _on_button_pressed() -> void:
+	rpc("rpc_button") 
+
+@rpc("any_peer","call_local")
+func rpc_button() :
+	print(player_id)
+	$Sprite2D.visible=true 
