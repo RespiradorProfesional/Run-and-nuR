334bbc3ba33a80d7fafd2cf3a7147760061d6b62
Flip <116906280+RespiradorProfesional@users.noreply.github.com>
2024-09-21 23:59:24 +0100

    solo amigos

--- run-and-nur/script/ui/main_menu.gd
+++ run-and-nur/script/ui/main_menu.gd
@@ -14,48 +14,25 @@ func _ready():
 	var api = Steam.steamInitEx(true, 480, true)
 	print(api)
 	Steam.join_requested.connect(_on_join_pressed)
-	Steam.lobby_match_list.connect(_on_lobby_match_list)
-	Steam.lobby_created.connect(_on_lobby_created)
 
 	scroll_container.visible = false
 	lobby_vbox.visible = false
 	background_lobby.visible = false
 
-func _on_lobby_created(connect: int, this_lobby_id: int) -> void:
-	if connect == 1:
-		lobby_id = this_lobby_id
-		print("Lobby creado: %s" % lobby_id)
-
-		# Conecta al lobby
-		peer.connect_lobby(lobby_id)
-		multiplayer.multiplayer_peer = peer
-
 func _on_host_lobby_pressed() -> void:
-	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC,2)
+	peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_FRIENDS_ONLY, 2)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	_on_peer_connected()
 
 func _on_join_pressed(lobby_id, steam_id) -> void:
+	print(lobby_id)
 	peer.connect_lobby(lobby_id)
 	multiplayer.multiplayer_peer = peer
 
 func _on_search_lobby_pressed() -> void:
-	# Configura el filtro de distancia para buscar lobbies
-	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
-	# Solicita la lista de lobbies
-	Steam.requestLobbyList()
-
-func _on_lobby_match_list(these_lobbies: Array) -> void:
-	# Limpia el contenedor de lobbi
-	
-	for lobby in these_lobbies:
-		var lobby_name: String = Steam.getLobbyData(lobby, "name")
-		var lobby_mode: String = Steam.getLobbyData(lobby, "mode")
-		var num_members: int = Steam.getNumLobbyMembers(lobby)
-
-		var lobby_button: Button = Button.new()
-		lobby_button.text = "Lobby: %s [%d/%d]" % [lobby_name, num_members, 2]
-		lobby_button.connect("pressed", Callable(self, "join_lobby").bind(lobby))
-		vbox_container.add_child(lobby_button)
-		scroll_container.visible=true
+	pass
 
 func join_lobby(lobby_id):
 	peer.connect_lobby(lobby_id)
