318dfdaddaf60764089c4178e84cdfc0e417ba23
Flip <116906280+RespiradorProfesional@users.noreply.github.com>
2024-09-23 09:51:48 +0100

    sadas

--- run-and-nur/script/levels/level_base.gd
+++ run-and-nur/script/levels/level_base.gd
@@ -12,18 +12,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if GlobalData.user_id==1:
-		var player_scene_1= load(GlobalData.chacter_player1_route)
-		var player_instantiate_1= player_scene_1.instantiate()
-		player_instantiate_1.name=str(multiplayer.get_unique_id())
-		player_instantiate_1.position=player_spawn.position
-		add_child(player_instantiate_1,true)
-	else:
-		var player_scene_2= load(GlobalData.chacter_player2_route)
-		var player_instantiate_2= player_scene_2.instantiate()
-		player_instantiate_2.name=str(multiplayer.get_unique_id())
-		player_instantiate_2.position=player_spawn.position
-		add_child(player_instantiate_2,true)
+	var player_scene= load("res://scene/characters/base_character.tscn")
+	var player_instantiate= player_scene.instantiate()
+	player_instantiate.name=str(multiplayer.get_unique_id())
+	player_instantiate.position=player_spawn.position
+	add_child(player_instantiate,true)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
