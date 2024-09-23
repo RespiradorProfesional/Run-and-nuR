9cb87de5a152fd87a0c2b8c4735371a1ff5e5192
Flip <116906280+RespiradorProfesional@users.noreply.github.com>
2024-09-23 08:52:42 +0100

    easd

--- run-and-nur/script/levels/level_base.gd
+++ run-and-nur/script/levels/level_base.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
 @onready var player_spawn=$player_spawn
+@onready var player_container=$player_container
 
 #EL PROBLEMA ESTA EN EL MULTIPLAYER SPAWN 
+#EL ORDEN AFECTA, YA QUE SI LO DEJO ASI PRIMERO EL HOST EL HOST NO VE AL USUARIO
+#PERO EL USUARIO A EL SI, PERO SI PRIMERO SE SPAWNEA EL USUARIO Y DESPUES EL PLAYER NO SE VEN
+#AUNQUE EL TEST SI FUNCIONA BIEN MIRAR A VER   
+
+#PROBAR SI QUITAR EL MARKER
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +18,13 @@ func _ready() -> void:
 		var player_instantiate_1= player_scene_1.instantiate()
 		player_instantiate_1.name=str(GlobalData.user_id)
 		player_instantiate_1.position=player_spawn.position
-		add_child(player_instantiate_1,true)
+		player_container.add_child(player_instantiate_1,true)
 	else:
 		var player_scene_2= load(GlobalData.chacter_player2_route)
 		var player_instantiate_2= player_scene_2.instantiate()
 		player_instantiate_2.name=str(GlobalData.user_id)
 		player_instantiate_2.position=player_spawn.position
-		add_child(player_instantiate_2,true)
+		player_container.add_child(player_instantiate_2,true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
