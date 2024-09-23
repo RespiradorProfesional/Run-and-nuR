2072a8718cd14fee7a675e0187288c37d0834d32
Flip <116906280+RespiradorProfesional@users.noreply.github.com>
2024-09-24 00:08:01 +0100

    prueba

--- run-and-nur/script/levels/level_base.gd
+++ run-and-nur/script/levels/level_base.gd
@@ -13,18 +13,19 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if GlobalData.user_id==1:
-		var player_scene_1= load(GlobalData.chacter_player1_route)
-		var player_instantiate_1= player_scene_1.instantiate()
-		player_instantiate_1.name=str(GlobalData.user_id)
-		player_instantiate_1.position=player_spawn.position
-		add_child(player_instantiate_1,true)
+	if get_tree().is_network_server():
+		# Instanciar jugadores solo en el servidor
+		var player_scene_1 = load(GlobalData.chacter_player1_route)
+		var player_instantiate_1 = player_scene_1.instantiate()
+		player_instantiate_1.name = str(GlobalData.user_id)
+		player_instantiate_1.position = player_spawn.position
+		add_child(player_instantiate_1, true)
 
-		var player_scene_2= load(GlobalData.chacter_player2_route)
-		var player_instantiate_2= player_scene_2.instantiate()
-		player_instantiate_2.name=str(GlobalData.user_id_2)
-		player_instantiate_2.position=player_spawn.position
-		add_child(player_instantiate_2,true)
+		var player_scene_2 = load(GlobalData.chacter_player2_route)
+		var player_instantiate_2 = player_scene_2.instantiate()
+		player_instantiate_2.name = str(GlobalData.user_id_2)
+		player_instantiate_2.position = player_spawn.position
+		add_child(player_instantiate_2, true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
