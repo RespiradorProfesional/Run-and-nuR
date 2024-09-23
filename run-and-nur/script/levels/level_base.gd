4fad800336893d1388717cf85113dc9c04f02453
Flip <116906280+RespiradorProfesional@users.noreply.github.com>
2024-09-23 09:48:15 +0100

    prueba_tonta

--- run-and-nur/script/levels/level_base.gd
+++ run-and-nur/script/levels/level_base.gd
@@ -12,27 +12,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	if GlobalData.user_id==1:
+		var player_scene_1= load(GlobalData.chacter_player1_route)
+		var player_instantiate_1= player_scene_1.instantiate()
+		player_instantiate_1.name=str(multiplayer.get_unique_id())
+		player_instantiate_1.position=player_spawn.position
+		add_child(player_instantiate_1,true)
+	else:
+		var player_scene_2= load(GlobalData.chacter_player2_route)
+		var player_instantiate_2= player_scene_2.instantiate()
+		player_instantiate_2.name=str(multiplayer.get_unique_id())
+		player_instantiate_2.position=player_spawn.position
+		add_child(player_instantiate_2,true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	if GlobalData.user_id==1:
-		var player_scene_1= load(GlobalData.chacter_player1_route)
-		var player_instantiate_1= player_scene_1.instantiate()
-		player_instantiate_1.name=str(GlobalData.user_id)
-		player_instantiate_1.position=player_spawn.position
-		add_child(player_instantiate_1,true)
-
-
-func _on_button_2_pressed() -> void:
-	if GlobalData.user_id!=1:
-		var player_scene_2= load(GlobalData.chacter_player2_route)
-		var player_instantiate_2= player_scene_2.instantiate()
-		player_instantiate_2.name=str(GlobalData.user_id)
-		player_instantiate_2.position=player_spawn.position
-		add_child(player_instantiate_2,true)
